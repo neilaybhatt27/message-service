63a7008e652e1cbe6445c2ee3e957c431758f9b0
Neilay Bhatt <neilaybhatt27@gmail.com>
2024-07-11 22:18:51 +0530

    Project initialized

---
+++ src/test/java/com/greatkapital/messageservice/MessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatkapital.messageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
