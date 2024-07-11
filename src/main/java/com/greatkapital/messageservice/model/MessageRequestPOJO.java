665c6e52dbc1d472aae71ab582feef10a27aa9a7
Neilay Bhatt <neilaybhatt27@gmail.com>
2024-07-12 00:47:07 +0530

    Added model package and added Message request POJO

---
+++ src/main/java/com/greatkapital/messageservice/model/MessageRequestPOJO.java
@@ -0,0 +1,12 @@
+package com.greatkapital.messageservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageRequestPOJO {
+    private String message;
+}
