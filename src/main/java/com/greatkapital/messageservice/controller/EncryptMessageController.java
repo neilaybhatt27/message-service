2cb04db8724240504cac564d12594aa585257351
Neilay Bhatt <neilaybhatt27@gmail.com>
2024-07-12 11:54:01 +0530

    Added Swagger documentaion for the POST API endpoint

--- src/main/java/com/greatkapital/messageservice/controller/EncryptMessageController.java
+++ src/main/java/com/greatkapital/messageservice/controller/EncryptMessageController.java
@@ -2,6 +2,11 @@ package com.greatkapital.messageservice.controller;
 
 import com.greatkapital.messageservice.model.MessageRequestPOJO;
 import com.greatkapital.messageservice.service.EncryptMessageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +36,28 @@ public class EncryptMessageController {
      * @param messageRequestPOJO MessageRequestPOJO object containing the input message.
      * @return Response Entity object containing the response body.
      */
+
+    @Operation(summary = "Encrypt the input message text and add it to the database.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Encryption and save successful",
+                    content = {@Content(mediaType = "application/json",
+                            examples = {@ExampleObject(value = """
+                            {
+                                "status": "success",
+                                "encryptedMessage": "5zb59KX/ZPp28KlYI5mA71Bh6D2ZQiKSh8WqtZBmMaNJZS5xsMxln74qaQlU0opd"
+                            }
+                            """)})}),
+            @ApiResponse(responseCode = "422",
+                    description = "Invalid input",
+                    content = {@Content(mediaType = "application/json",
+                            examples = {@ExampleObject(value = """
+                            {
+                                "errorObject": "Message cannot be null"
+                            }
+                            """)})})
+    })
+
     @PostMapping(path = "/message")
     public ResponseEntity<Object> encryptAndAddMessage(@RequestBody MessageRequestPOJO messageRequestPOJO) {
         LOGGER.info("IN EncryptMessageController.encryptAndAddMessage with MessageRequestPOJO: {}", messageRequestPOJO);
